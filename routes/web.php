e7e910d0fc07b8975dba1391921026dd2b12eaed
francesco mansi <f.mansi@socialwebsolutions.it>
2025-06-13 15:46:28 +0200

    Aggiunta lezione e teoria

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/chi-sono', function () {
+    return view('about');
+});
+
+Route::get('/contatti', function () {
+    return view('contact');
+});
